3524c853955dbf6de18543f2fb4ce3c4f669918d
andres <andres@algo.com>
2017-10-29 01:57:48 -0300

    'qweasdzxc'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_session!
+  before_action :authenticate_model!	
   def index
+  end	
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do  
-  get 'welcome/index'
+  
+  devise_for :sessions
+  devise_for :models
+  get 'welcome/index' => 'welcome#index', :as => 'welcome_index'
+  get 'welcome/contact'
   root 'welcome#index'
+  get '/categories/index' => 'old_urls#home'  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
