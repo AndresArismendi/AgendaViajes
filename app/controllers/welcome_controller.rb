8df48a19ac22825931577166fae3795a2a243ea2
andres <andres@algo.com>
2017-12-09 09:57:18 -0300

    'mod0912'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,13 @@ class WelcomeController < ApplicationController
   #before_action :authenticate_session!
   def index
   	@sessions = Session.all
+  	@trips = Trip.all
+    @trip_type = TripType.all
+    @passengers = Passenger.all
+    @guides = Session.all
+    
+
+
   end	
   def contact
   end
